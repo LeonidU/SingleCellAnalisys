6bf7f0d274f104a7cf5a8d52af3f62bbf8ab0fea
Leonid Uroshlev <leoniduroshlev@gmail.com>
2024-10-16 12:54:26 +0300

    Fix all bugs

--- SingleR_annotation.R
+++ SingleR_annotation.R
@@ -25,7 +25,9 @@ seurat_object <- FindNeighbors(seurat_object, dims = 1:10)
 seurat_object <- FindClusters(seurat_object, resolution = 0.5)
 all.markers <- FindAllMarkers(seurat_object)
 
-# Connect to Ensembl BioMart
+# Retrieve pig genes with their human orthologs
+# Note: We can retrieve homologs directly using the pig dataset
+
 ensembl <- useEnsembl(biomart = "genes")
 
 # List available datasets (optional)
@@ -40,21 +42,17 @@ pig_genes <- rownames(seurat_object)
 
 # Retrieve pig genes with their human orthologs
 # Note: We can retrieve homologs directly using the pig dataset
-
-gene_mapping <- getHomologs(pig_genes, species_from="pig", species_to="homo_sapiens")
-
-
-#gene_mapping <- getBM(
-#  attributes = c("ensembl_gene_id", "external_gene_name",
-#                 "hsapiens_homolog_ensembl_gene", "hsapiens_homolog_associated_gene_name"),
-#  filters = "external_gene_name",
-#  values = pig_genes,
-#  mart = ensembl_pig
-#)
+gene_mapping <- getBM(
+  attributes = c("ensembl_gene_id", "external_gene_name",
+                 "hsapiens_homolog_ensembl_gene", "hsapiens_homolog_associated_gene_name"),
+  filters = "ensembl_gene_id",
+  values = pig_genes,
+  mart = ensembl_pig
+)
 
 # Rename columns for clarity
-colnames(gene_mapping) <- c("pig_ensembl_gene_id",
-                            "human_ensembl_gene_id")
+colnames(gene_mapping) <- c("pig_ensembl_gene_id", "pig_gene_name",
+                            "human_ensembl_gene_id", "human_gene_name")
 
 # Remove entries without human orthologs
 gene_mapping <- gene_mapping[gene_mapping$human_gene_name != "", ]
@@ -62,23 +60,52 @@ gene_mapping <- gene_mapping[gene_mapping$human_gene_name != "", ]
 # Remove duplicate mappings based on pig gene names
 gene_mapping <- gene_mapping[!duplicated(gene_mapping$pig_gene_name), ]
 
-
 # Subset Seurat object to genes that have human orthologs
 common_genes <- intersect(rownames(seurat_object), gene_mapping$pig_gene_name)
 seurat_object <- subset(seurat_object, features = common_genes)
 
 # Create a named vector for mapping pig gene names to human gene names
-gene_map <- setNames(gene_mapping$human_gene_name, gene_mapping$pig_gene_name)
+#gene_map <- setNames(gene_mapping$human_gene_name, gene_mapping$pig_gene_name)
+ gene_map <- setNames(gene_mapping$human_gene_name, gene_mapping$pig_ensembl_gene_id)
 
-# Rename genes in Seurat object
-new_gene_names <- gene_map[rownames(seurat_object)]
-rownames(seurat_object@assays$RNA@counts) <- new_gene_names
-rownames(seurat_object@assays$RNA@data) <- new_gene_names
+counts_matrix <- GetAssayData(seurat_object, assay = "RNA", slot = "counts")
+
+# Map the row names (gene names) to human orthologs
+new_gene_names <- gene_map[rownames(counts_matrix)]
+
+# Remove any genes where the mapping resulted in NA
+valid_genes <- !is.na(new_gene_names)
+counts_matrix <- counts_matrix[valid_genes, ]
+new_gene_names <- new_gene_names[valid_genes]
+
+# Update row names with human gene names
+#rownames(counts_matrix) <- new_gene_names
+
+# Set the modified counts matrix back into the Seurat object
+#seurat_object <- SetAssayData(
+#  object = seurat_object,
+#  slot = "counts",
+#  new.data = counts_matrix
+#)
+
+# Repeat the process for the "data" slot (normalized data)
+data_matrix <- GetAssayData(seurat_object, assay = "RNA", slot = "scale.data")
+
+# Subset and rename the data matrix
+data_matrix <- data_matrix[valid_genes, ]
+rownames(data_matrix) <- new_gene_names
+
+# Set the modified data matrix back into the Seurat object
+#seurat_object <- SetAssayData(
+#  object = seurat_object,
+#  slot = "data",
+#  new.data = data_matrix
+#)
 
 # Load Human Primary Cell Atlas Data
 hpca.se <- celldex::HumanPrimaryCellAtlasData()
 # Extract the normalized data matrix
-data_matrix <- GetAssayData(seurat_object, slot = "data")
+#data_matrix <- GetAssayData(seurat_object, slot = "data")
 
 # Run SingleR
 singleR_results <- SingleR(test = data_matrix,
@@ -87,14 +114,27 @@ singleR_results <- SingleR(test = data_matrix,
                            de.method = "wilcox")
 
 # View results
-head(singleR_results$labels)
+#head(singleR_results$labels)
 # Add SingleR labels to Seurat metadata
 seurat_object <- AddMetaData(seurat_object, metadata = singleR_results$labels, col.name = "SingleR.labels")
 
 # If you have clusters and want to annotate clusters
-cluster_labels <- data.frame(cluster = levels(Idents(seurat_object)))
-cluster_labels$SingleR.labels <- singleR_results$pruned.labels
+#cluster_labels <- data.frame(cluster = levels(Idents(seurat_object)))
+#cluster_labels$SingleR.labels <- singleR_results$pruned.labels
 # Map cluster annotations back to Seurat object
-seurat_object@meta.data$SingleR.cluster.labels <- cluster_labels$SingleR.labels[match(Idents(seurat_object), cluster_labels$cluster)]
+#seurat_object@meta.data$SingleR.cluster.labels <- cluster_labels$SingleR.labels[match(Idents(seurat_object), cluster_labels$cluster)]
 # Save the updated Seurat object
-saveRDS(seurat_object, file = "seurat_object_annotated.rds")
+#saveRDS(seurat_object, file = "seurat_object_annotated.rds")
+
+library(anndata)
+new_list <- list()
+#It's kind of magic, but without that loading into h5ad doesn't work
+for (i in seq(1, length(singleR_results$pruned.labels))) {
+	new_list[as.character(i)] <- singleR_results$pruned.labels[i]
+}
+mat <- as.matrix(data_matrix)
+rownames(mat)[duplicated(rownames(mat))] <- paste0(rownames(mat)[duplicated(rownames(mat))], ".1")
+
+x <- AnnData(X=mat, uns=new_list, var=as.data.frame(seurat_object@active.ident))
+
+write_h5ad(x, "pigs.h5ad")
