08027323684cd53dffec1c03b1818b500a96f135
Leonid Uroshlev <leoniduroshlev@gmail.com>
2024-10-16 14:03:51 +0300

    Add command line arguments and fix bugs for data saving

--- SingleR_annotation.R
+++ SingleR_annotation.R
@@ -4,6 +4,7 @@ if (!require("SingleR")) BiocManager::install("SingleR")
 if (!require("celldex")) BiocManager::install("celldex")
 if (!require("biomaRt")) install.packages("biomaRt")
 if (!require("dplyr")) install.packages("dplyr")
+if (!require("anndata")) BiocManager::install("anndata")
 
 # Load libraries
 library(Seurat)
@@ -11,8 +12,33 @@ library(SingleR)
 library(celldex)
 library(biomaRt)
 library(dplyr)
+library(anndata)
 
-z<-ReadMtx(mtx="matrix.mtx", features="features.tsv", cells="barcodes.tsv",feature.column=1)
+check_files_exist <- function(dir) {
+  required_files <- c("matrix.mtx", "features.tsv", "barcodes.tsv")
+  existing_files <- list.files(path = dir)
+  
+  missing_files <- setdiff(required_files, existing_files)
+  
+  if (length(missing_files) == 0) {
+    message("All required files are present.")
+  } else {
+    stop(message("The following files are missing: ", paste(missing_files, collapse = ", ")))
+  }
+}
+
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) != 3) {
+	stop("Wrong number of arguments")
+}
+dir <- args[1]
+output_matrix_name <- args[2]
+
+check_files_exist(dir)
+
+
+# Standart Seurat pipeline for data processing
+z<-ReadMtx(mtx=paste0(dir, "/", "matrix.mtx"), features=paste0(dir, "/", "features.tsv"), cells=paste0(dir, "/", "barcodes.tsv"),feature.column=1)
 seurat_object <- CreateSeuratObject(counts = z, project = "HumanLiver")
 seurat_object <- NormalizeData(seurat_object, normalization.method = "LogNormalize", scale.factor = 10000)
 seurat_object <- FindVariableFeatures(seurat_object, selection.method = "vst", nfeatures = 2000)
@@ -21,19 +47,20 @@ seurat_object <- RunPCA(seurat_object, features = VariableFeatures(object = seur
 pcs_to_use <- 10
 # Run UMAP
 seurat_object <- RunUMAP(seurat_object, dims = 1:pcs_to_use)
-seurat_object <- FindNeighbors(seurat_object, dims = 1:10)
-seurat_object <- FindClusters(seurat_object, resolution = 0.5)
-all.markers <- FindAllMarkers(seurat_object)
+
+# SingleR make clustering by themself so we don't need that
+
+#seurat_object <- FindNeighbors(seurat_object, dims = 1:10)
+#seurat_object <- FindClusters(seurat_object, resolution = 0.5)
+#all.markers <- FindAllMarkers(seurat_object)
 
 # Retrieve pig genes with their human orthologs
 # Note: We can retrieve homologs directly using the pig dataset
 
 ensembl <- useEnsembl(biomart = "genes")
 
-# List available datasets (optional)
-# listDatasets(ensembl)
-
 # Set datasets for pig and human
+
 ensembl_pig <- useEnsembl(biomart = "genes", dataset = "sscrofa_gene_ensembl")
 ensembl_human <- useEnsembl(biomart = "genes", dataset = "hsapiens_gene_ensembl")
 
@@ -42,6 +69,8 @@ pig_genes <- rownames(seurat_object)
 
 # Retrieve pig genes with their human orthologs
 # Note: We can retrieve homologs directly using the pig dataset
+# Warning: Ensembl has very unstable servers. Could retrieve 500 error
+
 gene_mapping <- getBM(
   attributes = c("ensembl_gene_id", "external_gene_name",
                  "hsapiens_homolog_ensembl_gene", "hsapiens_homolog_associated_gene_name"),
@@ -66,7 +95,7 @@ seurat_object <- subset(seurat_object, features = common_genes)
 
 # Create a named vector for mapping pig gene names to human gene names
 #gene_map <- setNames(gene_mapping$human_gene_name, gene_mapping$pig_gene_name)
- gene_map <- setNames(gene_mapping$human_gene_name, gene_mapping$pig_ensembl_gene_id)
+gene_map <- setNames(gene_mapping$human_gene_name, gene_mapping$pig_ensembl_gene_id)
 
 counts_matrix <- GetAssayData(seurat_object, assay = "RNA", slot = "counts")
 
@@ -78,34 +107,17 @@ valid_genes <- !is.na(new_gene_names)
 counts_matrix <- counts_matrix[valid_genes, ]
 new_gene_names <- new_gene_names[valid_genes]
 
-# Update row names with human gene names
-#rownames(counts_matrix) <- new_gene_names
-
-# Set the modified counts matrix back into the Seurat object
-#seurat_object <- SetAssayData(
-#  object = seurat_object,
-#  slot = "counts",
-#  new.data = counts_matrix
-#)
-
 # Repeat the process for the "data" slot (normalized data)
-data_matrix <- GetAssayData(seurat_object, assay = "RNA", slot = "scale.data")
+# IMPORTANT! Numerical emperiments show what SingleR on scaled data give weird results. So I use regular data
+
+data_matrix <- GetAssayData(seurat_object, assay = "RNA", slot = "data")
 
 # Subset and rename the data matrix
 data_matrix <- data_matrix[valid_genes, ]
 rownames(data_matrix) <- new_gene_names
 
-# Set the modified data matrix back into the Seurat object
-#seurat_object <- SetAssayData(
-#  object = seurat_object,
-#  slot = "data",
-#  new.data = data_matrix
-#)
-
 # Load Human Primary Cell Atlas Data
 hpca.se <- celldex::HumanPrimaryCellAtlasData()
-# Extract the normalized data matrix
-#data_matrix <- GetAssayData(seurat_object, slot = "data")
 
 # Run SingleR
 singleR_results <- SingleR(test = data_matrix,
@@ -113,28 +125,23 @@ singleR_results <- SingleR(test = data_matrix,
                            labels = hpca.se$label.main,
                            de.method = "wilcox")
 
-# View results
-#head(singleR_results$labels)
-# Add SingleR labels to Seurat metadata
-seurat_object <- AddMetaData(seurat_object, metadata = singleR_results$labels, col.name = "SingleR.labels")
 
-# If you have clusters and want to annotate clusters
-#cluster_labels <- data.frame(cluster = levels(Idents(seurat_object)))
-#cluster_labels$SingleR.labels <- singleR_results$pruned.labels
-# Map cluster annotations back to Seurat object
-#seurat_object@meta.data$SingleR.cluster.labels <- cluster_labels$SingleR.labels[match(Idents(seurat_object), cluster_labels$cluster)]
-# Save the updated Seurat object
-#saveRDS(seurat_object, file = "seurat_object_annotated.rds")
-
-library(anndata)
 new_list <- list()
 #It's kind of magic, but without that loading into h5ad doesn't work
 for (i in seq(1, length(singleR_results$pruned.labels))) {
 	new_list[as.character(i)] <- singleR_results$pruned.labels[i]
 }
 mat <- as.matrix(data_matrix)
-rownames(mat)[duplicated(rownames(mat))] <- paste0(rownames(mat)[duplicated(rownames(mat))], ".1")
+vars <- as.data.frame(as.integer(as.factor(singleR_results$pruned.labels)))
+rownames(vars) <- rownames(singleR_results)
+colnames(vars) <- "Cluster's Number"
 
-x <- AnnData(X=mat, uns=new_list, var=as.data.frame(seurat_object@active.ident))
+i <- 1
 
-write_h5ad(x, "pigs.h5ad")
+while (sum(duplicated(rownames(mat))) > 0) {
+	rownames(mat)[duplicated(rownames(mat))] <- paste0(rownames(mat)[duplicated(rownames(mat))], ".", as.character(i))
+	i <- i+1
+}
+x <- AnnData(X=mat, uns=new_list, var=vars)
+
+write_h5ad(x, output_matrix_name)
