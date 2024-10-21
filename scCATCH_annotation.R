66a4484f1309f4ed0d8e5bf89bb2b257ad61aa13
Leonid Uroshlev <leoniduroshlev@gmail.com>
2024-10-21 19:35:04 +0300

    Add files via upload
    Obtaining clusters annotation (actually very bad)

--- scCATCH_annotation.R
+++ scCATCH_annotation.R
@@ -34,20 +34,23 @@ output_matrix_name <- args[2]
 
 check_files_exist(dir)
 
-
+options(Seurat.object.assay.version = "v3")
 z<-ReadMtx(mtx=paste0(dir, "/", "matrix.mtx"), features=paste0(dir, "/", "features.tsv"), cells=paste0(dir, "/", "barcodes.tsv"),feature.column=1)
 seurat_object <- CreateSeuratObject(counts = z, project = "Lybestky_project")
 seurat_object <- NormalizeData(seurat_object, normalization.method = "LogNormalize", scale.factor = 10000)
 seurat_object <- FindVariableFeatures(seurat_object, selection.method = "vst", nfeatures = 2000)
 seurat_object <- ScaleData(seurat_object, features = rownames(seurat_object))
-seurat_object <- RunPCA(seurat_object, features = VariableFeatures(object = seurat_object))
+
 pcs_to_use <- 10
 # Run UMAP
-seurat_object <- RunUMAP(seurat_object, dims = 1:pcs_to_use)
-seurat_object <- FindNeighbors(seurat_object, dims = 1:10)
+#seurat_object <- RunUMAP(seurat_object, dims = 1:pcs_to_use)
+#seurat_object <- FindNeighbors(seurat_object, dims = 1:10)
 #seurat_object <- FindClusters(seurat_object, resolution = 0.5)
 
 # Set dataset for pig
+# Most dangerous place in script
+# Possible solving of problem:
+# 1. Failed to collect lazy table. - devtools::install_version("dbplyr", version = "2.3.4")
 ensembl_pig <- useEnsembl(biomart = "genes", dataset = "sscrofa_gene_ensembl", host="https://may2024.archive.ensembl.org")
 
 # Get pig genes from your Seurat object
@@ -83,23 +86,22 @@ expr_matrix <- GetAssayData(seurat_object, assay = "RNA", slot = "counts")
 # Rename genes in Seurat object
 new_gene_names <- gene_map[rownames(seurat_object)]
 rownames(expr_matrix) <- new_gene_names
-rownames(seurat_object@assays$RNA$data) <- new_gene_names
-rownames(seurat_object@assays$RNA$count) <- new_gene_names
+rownames(seurat_object@assays$RNA@data) <- new_gene_names
+rownames(seurat_object@assays$RNA@count) <- new_gene_names
 
 
 # Remove any genes with NA after mapping (if any)
-valid_genes <- !is.na(rownames(expr_matrix))
+valid_genes <- !is.na(rownames(seurat_object))
 seurat_object <- subset(seurat_object, features = rownames(seurat_object)[valid_genes])
 
 # Ensure clusters are defined
 # If not already clustered, perform clustering
-# seurat_object <- FindNeighbors(seurat_object, dims = 1:10)
-# seurat_object <- FindClusters(seurat_object, resolution = 0.5)
+seurat_object <- RunPCA(seurat_object, features = VariableFeatures(object = seurat_object))
+seurat_object <- FindNeighbors(seurat_object, dims = 1:10)
+seurat_object <- FindClusters(seurat_object, resolution = 0.5)
 
 # Find marker genes for each cluster
 markers <- FindAllMarkers(seurat_object, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-all.markers <- FindAllMarkers(seurat_object)
-
 # View the markers
 head(markers)
 # Prepare marker gene list for scCATCH
@@ -110,27 +112,25 @@ marker_list <- split(markers, markers$cluster)
 #expr_matrix <- as.matrix(seurat_object@assays$RNA@data)
 
 # Initialize scCATCH object
-scCATCH_obj <- createscCATCH(data = expr_matrix, cluster = seurat_object@meta.data$seurat_clusters)
+expr_matrix <- as.matrix(seurat_object@assays$RNA@data)
+
+# Initialize scCATCH object
+scCATCH_obj <- createscCATCH(data = expr_matrix, cluster = as.character(seurat_object@meta.data$seurat_clusters))
+
 
 # Add marker genes to scCATCH object
-scCATCH_obj@markers <- marker_list
+#scCATCH_obj@markers <- marker_list
 # Set species and tissue for annotation
 species <- "Human"
 tissue <- "Liver"
+
+obj <- findmarkergene(object=scCATCH_obj, species=species, tissue=tissue, marker = cellmatch)
 # Identify cell types
-scCATCH_obj <- findmarkergene(object = scCATCH_obj,
-                              species = species,
-                              marker = cellmatch,
-                              tissue = tissue,
-                              use_method = "median",
-                              marker_list = NULL)
-
-# View the cell type annotations
-celltype_results <- scCATCH_obj@celltype
-# Create a data frame mapping clusters to predicted cell types
-cluster_annotations <- data.frame(cluster = celltype_results$cluster,
-                                  cell_type = celltype_results$cell_type,
-                                  stringsAsFactors = FALSE)
-
-# Add cell type annotations to Seurat object metadata
-seurat_object@meta.data$scCATCH_cell_type <- cluster_annotations$cell_type[match(seurat_object@meta.data$seurat_clusters, cluster_annotations$cluster)]
+#scCATCH_obj <- findmarkergene(object = scCATCH_obj,
+#                              species = species,
+#                              marker = cellmatch,
+#                              tissue = tissue,
+#                              use_method = "median")
+obj2 <- findcelltype(obj)
+obj2@celltype <- obj2@celltype[order(as.integer(obj2@celltype$cluster)), ]
+library(anndata)
